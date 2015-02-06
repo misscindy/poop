dfa27274a3c1a24385485a6d23cdbfcd43ecb120
Bingxin Fan <bf776@nyu.edu>
2015-02-06 14:50:15 -0800

    reverse sub linked list, EPI8.3 + Leetcode

--- EPI_8.3.py
+++ EPI_8.3.py
@@ -8,39 +8,29 @@ from util import *
 
 
 def reverseBetween(head, m, n):
-    if not head: return head 
-    counter, prev, tail_next = 1, head, head
-    while counter < m - 1:
+    if not head or n == m: return head 
+    dummy = ListNode(-1)
+    dummy.next = head 
+    counter, prev = 1, dummy
+    while counter < m:
         prev = prev.next
         counter += 1 
-
+    
     h, t, r = _reverse(prev.next, prev.next.next, counter + 1, n)
     prev.next = h
-    t.next = r
-    # tail.next = prev.next 
-    # prev.next = prev_next
-    return head 
-
-
-
-def reverse_ll(head):
-
-	if not head: return None 
-	head, _ = _reverse(head, head.next) 
-	return head 
-
-
-def _reverse(cur_node, next_node, counter, n):
-    if counter == n:
-        return cur_node, cur_node, cur_node.next
-    head, tail, r = _reverse(next_node, next_node.next, counter + 1, n)
-    tail.next = cur_node
-    tail = cur_node
-    tail.next = None 
-    return head, tail, r
-
-
-
+    t.next = r 
+    return dummy.next 
+    
+def _reverse(current, nxt, counter, n):
+    if counter == n + 1:
+        return current, current, nxt 
+    head, tail, remainder = _reverse(current.next, nxt.next, counter + 1, n)
+    tail.next = current
+    tail = tail.next 
+    current.next = None 
+    return head, tail, remainder  
+    
+    
 
 if __name__ == '__main__':
     test_cases = [
@@ -51,7 +41,9 @@ if __name__ == '__main__':
     for ((values1, m, n), values2) in test_cases:
         l1, l2 = createLinkedList(values1), createLinkedList(values2)
         head = reverseBetween(l1, m, n)
-        print isSameLinkedList(l1, l2)
+        printLinkedList(head)
+        printLinkedList(l2)
+        print isSameLinkedList(head, l2)
 
 	# values = [1, 3, 5, 7, 9]
 	# ll = createLinkedList(values)
